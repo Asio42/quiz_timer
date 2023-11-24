e9a80ff54d45283d84b5cc75c5792ef6aded3a2a
Asio42 <marcelolopez4242@gmail.com>
2023-11-23 22:55:43 -0300

    save project from gitlab

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./_variables.sass";
+import "./index.sass";
+import Timer from "./Timer";
+import { StateProvider } from "./contexts/stateProvider";
+import reducer, { initialState } from "./reducer";
+import ThemeProvider from "./contexts/themeProvider";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <StateProvider reducer={reducer} initialState={initialState}>
+      <ThemeProvider>
+        <Timer />
+      </ThemeProvider>
+    </StateProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
